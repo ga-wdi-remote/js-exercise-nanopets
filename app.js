58fd9b631ace6c851797932b5d732e47ebb4dcb5
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-03-17 12:22:38 -0400

    new solution

--- app.js
+++ app.js
@@ -3,37 +3,14 @@ var nanoPet = {
   foodInTummy: 0
 };
 
-var start = function(){
-    nanoPet.name = "Gertrude";
-    nanoPet.foodInTummy = 0;
+var namePet = function(){
+    nanoPet.name = prompt("What is your pet's new name?");
     showMenu();
 };
 
-var showStatus = function(){
-    alert(nanoPet.name + " has " + nanoPet.foodInTummy + " food in its tummy");
-};
-
-var showMenu = function(){
-    showStatus();
-    var choice = prompt("What do you want to do? \n 1: name pet \n 2: feed pet \n 3: exercise pet");
-    console.log(typeof choice);
-    if(choice === "1"){
-        namePet();
-    } else if (choice === "2"){
-        feedPet();
-    } else if (choice === "3"){
-        exercisePet();
-    }
-};
-
-var namePet = function(){
-  nanoPet.name = prompt("What is your pet's new name?");
-  showMenu();
-};
-
 var feedPet = function(){
-  nanoPet.foodInTummy += 5;
-  showMenu();
+    nanoPet.foodInTummy += 5;
+    showMenu();
 };
 
 var exercisePet = function(){
@@ -41,4 +18,21 @@ var exercisePet = function(){
   showMenu();
 };
 
-start();
+var showStatus = function(){
+  alert(nanoPet.name + " has " + nanoPet.foodInTummy + " food in its tummy");
+}
+
+var showMenu = function(){
+  showStatus();
+  var choice = prompt("What do you want to do? \n 1. name pet \n 2. feed pet \n 3. exercise pet");
+
+  if (choice === "1"){
+    namePet();
+  } else if (choice === "2") {
+    feedPet();
+  } else if (choice === "3"){
+    exercisePet();
+  }
+};
+
+showMenu();
